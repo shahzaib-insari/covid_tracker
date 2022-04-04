fd9b3d725f977ca0c3848793c71e862809d4ee9c
Shahzaib Arshad <43374733+shahzaib-insari@users.noreply.github.com>
2022-04-04 16:47:49 +0500

    Added shimmer effect and search functionality

--- lib/Views/countries_stats.dart
+++ lib/Views/countries_stats.dart
@@ -1,5 +1,6 @@
 import 'package:covid_tracker/Services/stats_services.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CountriesStatsScreen extends StatefulWidget {
   const CountriesStatsScreen({Key? key}) : super(key: key);
@@ -49,26 +50,79 @@ class _CountriesStatsScreenState extends State<CountriesStatsScreen> {
                 future: statsServices.fetchCountriesStats(),
                 builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
                   if (!snapshot.hasData) {
-                    return Text("Loading.....");
+                    return ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, index) {
+                        return Shimmer.fromColors(
+                          child: Column(
+                            children: [
+                              ListTile(
+                                title: Container(
+                                  height: 10,
+                                  width: 80,
+                                  color: Colors.white,
+                                ),
+                                subtitle: Container(
+                                  height: 10,
+                                  width: 80,
+                                  color: Colors.white,
+                                ),
+                                leading: Container(
+                                  height: 50,
+                                  width: 50,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                          baseColor: Colors.grey.shade700,
+                          highlightColor: Colors.grey.shade100,
+                        );
+                      },
+                    );
                   } else {}
                   return ListView.builder(
                     itemCount: snapshot.data?.length,
                     itemBuilder: (context, index) {
-                      return Column(
-                        children: [
-                          ListTile(
-                            title: Text(snapshot.data?[index]['country']),
-                            leading: Image(
-                              height: 50,
-                              width: 50,
-                              image: NetworkImage(
-                                  snapshot.data?[index]['countryInfo']['flag']),
-                            ),
-                            subtitle:
-                                Text(snapshot.data![index]['cases'].toString()),
-                          )
-                        ],
-                      );
+                      String name = snapshot.data![index]['country'];
+
+                      if (searchController.text.isEmpty) {
+                        return Column(
+                          children: [
+                            ListTile(
+                              title: Text(snapshot.data?[index]['country']),
+                              leading: Image(
+                                height: 50,
+                                width: 50,
+                                image: NetworkImage(snapshot.data?[index]
+                                    ['countryInfo']['flag']),
+                              ),
+                              subtitle: Text(
+                                  snapshot.data![index]['cases'].toString()),
+                            )
+                          ],
+                        );
+                      } else if (name
+                          .toLowerCase()
+                          .contains(searchController.text.toLowerCase())) {
+                        return Column(
+                          children: [
+                            ListTile(
+                              title: Text(snapshot.data?[index]['country']),
+                              leading: Image(
+                                height: 50,
+                                width: 50,
+                                image: NetworkImage(snapshot.data?[index]
+                                    ['countryInfo']['flag']),
+                              ),
+                              subtitle: Text(
+                                  snapshot.data![index]['cases'].toString()),
+                            )
+                          ],
+                        );
+                      } else {
+                        return Container();
+                      }
                     },
                   );
                 },
